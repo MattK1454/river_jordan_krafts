fe0873fe5a94e8b50a8a9bbdd2049c0d3c4a8be6
Matthew Kaufmann <matthewpkauf@gmail.com>
2021-09-13 21:51:02 -0500

    Connecting To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -14,11 +17,14 @@ app.get('/api/products', (request, response) => {
   response.json(products);
 });
 
-app.get("/api/products/:id", (request, response) => {
-    const product = products.find(product => product._id === request.params.id);
-    response.json(product);
-})
+app.get('/api/products/:id', (request, response) => {
+  const product = products.find((product) => product._id === request.params.id);
+  response.json(product);
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
